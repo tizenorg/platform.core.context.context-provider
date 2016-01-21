adf87a3071a37e167835d1b5b266a6a0e37b4b32
Somin Kim <somin926.kim@samsung.com>
2016-01-21 13:58:06 +0900

    Added custom provider
    Change-Id: I4fee414b0d952b02e0256bf71f6e4a7c05593167
    Signed-off-by: Somin Kim <somin926.kim@samsung.com>

---
+++ src/custom/custom_base.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <context_mgr.h>
+#include "custom_base.h"
+
+ctx::custom_base::custom_base(std::string subject, std::string name, ctx::json tmpl, std::string owner)
+: _subject(subject), _name(name), _tmpl(tmpl), _owner(owner)
+{
+}
+
+ctx::custom_base::~custom_base()
+{
+}
+
+bool ctx::custom_base::is_supported()
+{
+	return true;
+}
+
+void ctx::custom_base::submit_trigger_item()
+{
+	context_manager::register_trigger_item(_subject.c_str(), OPS_SUBSCRIBE | OPS_READ,
+			_tmpl.str(), NULL, _owner.c_str());
+}
+
+void ctx::custom_base::unsubmit_trigger_item()
+{
+	context_manager::unregister_trigger_item(_subject.c_str());
+}
+
+int ctx::custom_base::subscribe(const char *subject, ctx::json option, ctx::json *request_result)
+{
+	return ERR_NONE;
+
+}
+
+int ctx::custom_base::unsubscribe(const char *subject, ctx::json option)
+{
+	return ERR_NONE;
+}
+
+int ctx::custom_base::read(const char *subject, ctx::json option, ctx::json *request_result)
+{
+	ctx::json data = latest.str();
+	ctx::context_manager::reply_to_read(_subject.c_str(), NULL, ERR_NONE, data);
+	return ERR_NONE;
+}
+
+int ctx::custom_base::write(const char *subject, ctx::json data, ctx::json *request_result)
+{
+	return ERR_NONE;
+}
+
+void ctx::custom_base::handle_update(ctx::json data)
+{
+	// Store latest state
+	latest = data.str();
+	ctx::context_manager::publish(_subject.c_str(), NULL, ERR_NONE, data);
+}
+
+const char* ctx::custom_base::get_subject()
+{
+	return _subject.c_str();
+}
+
+std::string ctx::custom_base::get_owner()
+{
+	return _owner;
+}
+
+ctx::json ctx::custom_base::get_template()
+{
+	return _tmpl;
+}
