402cb3e95f3860f17344501d0835e1b94349326d
Mu-Woong Lee <muwoong.lee@samsung.com>
2016-04-27 21:04:54 +0900

    Break the direct dependency to media-content from the media stats provider
    If necessary, the provider dynamically load another so to use the media content API.
    
    Change-Id: Id7f1fab0e956ee7702382328b981860f5fa6c9fc
    Signed-off-by: Mu-Woong Lee <muwoong.lee@samsung.com>

---
+++ src/media-stats/util/MediaInfoUtil.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <media_content.h>
+#include <Types.h>
+
+extern "C" SO_EXPORT bool getMediaPlayCount(int updateItem, int updateType, int mediaType, char *uuid, int *count)
+{
+	IF_FAIL_RETURN(updateItem == MEDIA_ITEM_FILE && updateType == MEDIA_CONTENT_UPDATE, false);
+	IF_FAIL_RETURN(mediaType == MEDIA_CONTENT_TYPE_MUSIC || mediaType == MEDIA_CONTENT_TYPE_VIDEO, false);
+
+	media_info_h media = NULL;
+	media_info_get_media_from_db(uuid, &media);
+	IF_FAIL_RETURN_TAG(media, false, _E, "media_info_get_media_from_db() failed");
+
+	media_info_get_played_count(media, count);
+	media_info_destroy(media);
+
+	return true;
+}
